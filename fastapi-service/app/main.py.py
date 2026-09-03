a454c5f50182c941f66e7a6c4e8228864f1ac69b
rameshs <ramsbook@gmail.com>
2026-09-03 13:56:56 -0400

    Initial commit: FastAPI + Flink claim processing system

---
+++ fastapi-service/app/main.py.py
@@ -0,0 +1,13 @@
+mkdir C:\Users\ramsk\ibm\claim-processing-flink-fastapi\claim-processing-flink-fastapi
+cd C:\Users\ramsk\ibm\claim-processing-flink-fastapi\claim-processing-flink-fastapi
+
+mkdir fastapi-service
+mkdir fastapi-service\app
+
+mkdir flink-job
+mkdir flink-job\src
+mkdir flink-job\src\main
+mkdir flink-job\src\main\java
+mkdir flink-job\src\main\java\com
+mkdir flink-job\src\main\java\com\ramsusa
+mkdir flink-job\src\main\java\com\ramsusa\claims
